0399bb8feb013a052ecb89f468c7ed0f29b3dad4
SMRMousavi <mrmousavi78@gmail.com>
2020-12-15 11:01:20 +0330

    Add Farsi language to bot languages

---
+++ texts.py
@@ -0,0 +1,229 @@
+
+texts = {
+
+#'language':{
+#    'en':'Hi, please choose your prefered language.'
+#    'fa':'سلام لطفا زبان دلخواه خود را انتخاب کنید.'
+#    },
+
+'start': {
+    'en':
+'''
+Hi, Welcome to <B>top-up gifter bot</B>.
+If you did not receive free 5,000 IRT top-up you can follow steps to receive your prize.
+You can also look at all steps by pressing "Help" button.
+Please notice that you can only receive this prize once.
+
+⚠️: <B>For now you can receive your free top-up just for Irainian sim cards</B>.
+
+'''
+        ,
+    'fa':
+'''
+سلام به <B>ربات شارژ هدیه</B> خوش آمدید.
+اگر تا به حال از این ربات شارژ رایگان دریافت نکرده اید، برای دریافت شارژ ۵ هزار تومانی رایگان مراحل را با دقت انجام دهید.
+برای مشاهده تمامی مراحل کلید راهنما را فشار دهید.
+توجه داشته باشید که این هدیه تنها یکبار قابل دریافت است.
+'''
+
+},
+
+
+'BrightID_verify': {
+    'en':
+
+'''
+If you downloaded BrightID app then do follow these steps and press "Next" button:
+
+    - Create an account in BrightID app.
+    - Verify your account in BrightID.
+
+Note that you should verify your BrightID account by participating in BrightID meet.
+Below is the link you can check upcoming meetings:
+https://meet.brightid.org/#/
+'''
+,
+    
+    'fa': 
+'''
+
+بعد از بارگیری و نصب اپلیکشن BrightID مراحل زیر را انجام دهید و سپس کلید “بعدی” را فشار دهید.
+
+	- یک حساب در اپلیکشن BrightID بسازید.
+	- با شرکت در جلسات احراز هویت BrightID حساب خود را احراز هویت کنید.
+
+توجه داشته باشید که تنها با شرکت در جلسات احراز هویت می توانید حساب خود را احراز هویت کنید.
+
+از لینک زیر می توانید ساعت های برگزاری این جلسات را مشاهده کنید:
+https://meet.brightid.org/#/
+'''
+},
+
+
+'link_account': {
+
+    'en':
+'''
+Now you should link your BrightID account to our app.
+
+Please press "Link account" button to do so or scan the QR code with your mobile.
+''',
+
+    'fa':
+'''
+حالا باید حساب خود را به برنامه ما لینک کنید.
+برای این کار کلید “اتصال حساب” را فشار دهید و یا با گوشی خود QR کد ارسال شده را اسکن کنید.
+'''
+},
+
+
+'help': {
+    'en':
+'''
+Receiving SIM Card credit steps:
+1. Install BrightID app.
+2. Create an account in BrightID app.
+3. Verify your account in BrightID.
+4. Link BrightID account with telegram verification bot throught the DeepLink you received.
+5. Request Free credit.
+''',
+
+    'fa':
+'''
+مراحل دریافت شارژ رایگان:
+۱. نصب برنامه BrightID
+۲. ساخت حساب در برنامه BrightID
+۳. تایید هویت حساب ساخته شده
+۴. لینک کردن حساب ساخته شده با برنامه ما
+۵. درخواست شارژ رایگان در ربات
+
+'''
+},
+
+'phone_number':{
+    'en': "Please enter the phone number you want the prize for like the sample.\nSample: 09031000000"
+,
+
+    'fa': "لطفا مطابق نمونه شماره تلفن خود را به زبان انگلیسی وارد کنید.\nنمونه: 09031000000"
+},
+
+'operator':{
+    'en':
+'''
+Please choose your SIM Card operator:
+''',
+
+    'fa':
+'''
+لطفا اپراتور سیم کارت خود را انتخاب کنید:
+'''
+},
+
+'phone_submitted':{
+    'en':
+'''
+Phone number successfully submitted.
+Press "Receive" button to get your prize.
+''',
+
+    'fa':
+'''
+شمارت تلفن با موفقیت ثبت شد.
+برای دریافت هدیه کلید "دریافت" را فشار دهید.
+'''
+},
+
+'download_links':{
+    'en':
+'''
+Here the links to download app.
+''',
+
+    'fa':
+'''
+از طریق لینک های زیر میتوانید برنامه را بارگیری کنید.
+'''
+},
+
+'receive':{
+    'en': 'Now if you are verified you can receive you prize.',
+
+    'fa': 'اگر حساب شما تایید هویت شده است می توانید هدیه خود را دریافت کنید.'
+},
+
+'finish': {
+    'en':
+'''Congratulation.
+
+Prize claimed.
+
+Here is your supporting code: 
+''',
+
+    'fa':
+'''
+تبریک
+
+هدیه دریافت شد.
+
+کد پشتیبانی: 
+'''
+},
+
+'errors':{
+    'again':{
+        'en': '***Try again***',
+
+        'fa': '***دوباره تلاش کنید***'
+    },
+
+    'link':{
+        'en': 
+'''
+Sorry but you did not link your BrightID account to our app.
+Do the previous steps to link it.
+''',
+
+        'fa':
+'''
+متاسفانه شما حساب BrightID خود را به برنامه ما لینک نکرده اید.
+مراحل قبل را مجددا انجام دهید.
+'''
+    },
+
+    'verify':{
+        'en':
+'''
+'Sorry but you are not verified.'
+''',
+        'fa':
+'''
+متاسفانه هویت شما در BrihgtID تایید نشده است.
+'''
+    },
+    
+    'awarded':{
+        'en':
+'''
+'Sorry but you received the prize once.'
+''',
+        'fa':
+'''
+شما یکبار هدیه را دریافت کرده اید.
+'''
+    },
+
+    'our_side':{
+        'en': 
+'''
+Something happend in our side.
+Please try again or contact customer support.
+''',
+        'fa':
+'''
+مشگلی در سمت ما پیش آمده است.
+لطفا مجددا تلاش کنید و یا با پشتیانی تماس بگیرید.
+'''
+    }
+}
+}
